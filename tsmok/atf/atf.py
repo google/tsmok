46b3c91f53fbc5bb3fac0d666907613730b7b424
Dmitry Yatsushkevich <dmitryya@google.com>
2021-01-05 23:15:02 +0000

    atf: small refactor
    Minor refactor:
      - rename param from 'os' to 'tee'
      - remove unneeded logs
    
    Change-Id: I3c5414c6222886b65ddd39fd1ffe11e0b6383017

--- tsmok/atf/atf.py
+++ tsmok/atf/atf.py
@@ -30,32 +30,30 @@ class Atf:
         self.os_return_call_done
 
   def _args_dump(self, args: List[int]) -> None:
-    self._log.debug('Args:')
+    self._log.info('Args:')
     for i in range(len(args)):
-      self._log.debug('\targs[%d]: 0x%08x', i, args[i])
+      self._log.info('\targs[%d]: 0x%08x', i, args[i])
 
-  def smc_handler(self, os, flag, call, args):
+  def smc_handler(self, tee, flag, call, args):
     try:
-      self._log.info('.exec. => SMC call 0x%08x', call)
-      ret = self._callbacks[call](os, flag, args)
-      self._log.info('.exec. <= SMC call 0x%08x: 0x%08x', call, ret)
+      self._log.info('.exec. => SMC: 0x%08x', call)
+      ret = self._callbacks[call](tee, flag, args)
+      self._log.info('.exec. <= SMC: 0x%08x ret 0x%08x', call, ret)
     except KeyError:
       raise error.Error(f'Unhandled Smc call (0x{call:08x}).')
 
     return ret
 
-  def os_return_entry_done(self, os, flag, args):
+  def os_return_entry_done(self, tee, flag, args):
     if not flag & atf_const.SmcCallFlag.SECURE:
       raise error.Error('OS return calls are not supported in NS mode')
 
-    self._args_dump(args)
-    os.set_atf_vector_table_addr(args[0])
+    tee.set_atf_vector_table_addr(args[0])
 
-    os.exit(atf_const.SmcErrorCode.OK)
+    tee.exit(atf_const.SmcErrorCode.OK)
     return atf_const.SmcErrorCode.OK
 
-  def os_return_call_done(self, os, flag, args):
+  def os_return_call_done(self, tee, flag, args):
     del flag  # not used in this call
-    self._args_dump(args)
-    os.exit(args[0], args[1], args[2], args[3])
-    return args[0]
+    tee.exit(args[0], args[1], args[2], args[3])
+    return atf_const.SmcErrorCode.OK
