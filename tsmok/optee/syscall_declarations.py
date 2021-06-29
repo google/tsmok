8c6c5c8d2666eb71038b909ffae8563dd0984d96
Dmitry Yatsushkevich <dmitryya@google.com>
2021-06-29 22:19:54 +0000

    optee: Update syscall parser
    Update syscall parser to support ARRAY and OPTEE Time, ObjectInfo,
    UUID and UteeAttribute arguments.
    
    Change-Id: Ied0a1961730f820ae6f70c5ab69b0d4ad9ee9b82

--- tsmok/optee/syscall_declarations.py
+++ tsmok/optee/syscall_declarations.py
@@ -3,67 +3,65 @@
 import tsmok.optee.syscall_parser as parser
 
 TA_SYSCALL_DECLARATIONS = """
-"open_session" 0 ("sid" int32 [ __out_res__ ], "param" optee_utee_param_ptr) void
-"invoke_command" 2 ("sid" int32 [ __in_res__ ], "param" optee_utee_param_ptr, "cmd" int32) void
+"open_session" 0 ("sid" int32 [ __out_res__ ], "param" utee_param_ptr [ __out__ ]) void
+"invoke_command" 2 ("sid" int32 [ __in_res__ ], "param" utee_param_ptr [ __out__ ], "cmd" int32) void
 "close_session" 1 ("sid" int32 [ __in_res__ ]) void
 """
 
 TEE_SYSCALL_DECLARATIONS = """
-"sys_return" 0 ("ret" int32) void
-"log" 1 ("buf" void_ptr, "len" int32) void
-"panic" 2 ("code" int32) void
-"get_property" 3 ("prop_set" int32, "index" int32, "name" void_ptr, "name_len" int32_ptr, "buf" void_ptr, "blen" int32_ptr, "prop_type" int32_ptr) int32
-"get_property_name_to_index" 4 ("prop_set" int32, "name" void_ptr, "name_len" int32, "index" int32_ptr) int32
-"open_ta_session" 5 (TEE_UUID *dest, "cancel_req_to" int32, struct utee_params *usr_param, "ta_sess" int32_ptr, "ret_orig" int32_ptr) int32
-"close_ta_session" 6 ("ta_sess" int32) int32
-"invoke_ta_command" 7 ("ta_sess" int32, "cancel_req_to" int32, "cmd_id" int32, struct utee_params *usr_param, "ret_orig" int32_ptr) int32
-"check_access_rights" 8 ("flags" int32, "buf" void_ptr, "len" int32) int32
+"log" 1 ("buf" void_ptr [__array__], "len" int32 [__array_len__]) void
+"get_property" 3 ("prop_set" int32, "index" int32 [ __in_res__ ], "name" void_ptr [ __array__ ], "name_len" int32_ptr [ __array_len__], "buf" void_ptr [ __array__ ], "buf_len" int32_ptr [ __array_len__ ], "prop_type" int32_ptr) int32
+"get_property_name_to_index" 4 ("prop_set" int32, "name" void_ptr [ __array__ ], "name_len" int32 [ __array_len__ ], "index" int32_ptr [ __out_res__ ]) int32
+"open_ta_session" 5 ("uuid_dest" uuid_ptr, "cancel_req_to" int32, "param" utee_param_ptr, "ta_sess" int32_ptr [ __out_res__ ], "ret_orig" int32_ptr) int32
+"close_ta_session" 6 ("ta_sess" int32 [ __in_res__ ]) int32
+"invoke_ta_command" 7 ("ta_sess" int32 [ __in_res__ ], "cancel_req_to" int32, "cmd_id" int32, "param" utee_param_ptr, "ret_orig" int32_ptr) int32
+"check_access_rights" 8 ("flags" int32, "buf" void_ptr [ __array__ ], "buf_len" int32 [ __array_len__ ]) int32
 "get_cancellation_flag" 9 ("cancel" int32_ptr) int32
-"unmask_cancellation" 10 ("old_mask" int32_ptr) int32;
-"mask_cancellation" 11 ("old_mask" int32_ptr) int32;
-"wait" 12 ("timeout" int32) int32;
-"get_time" 13 ("cat" int32, TEE_Time *time) int32;
-"set_ta_time" 14 (TEE_Time *mytime) int32
-"cryp_state_alloc" 15 ("algo" int32, "mode" int32, "key1" int32, "key2" int32, "state" int32_ptr) int32
-"cryp_state_copy" 16 ("dst" int32, "src" int32) int32
-"cryp_state_free" 17 ("state" int32) int32
-"hash_init" 18 ("state" int32, "iv" void_ptr, "iv_len" int32) int32
-"hash_update" 19 ("state" int32, "chunk" void_ptr, "chunk_size" int32) int32
-"hash_final" 20 ("state" int32, "chunk" void_ptr, "chunk_size" int32, "hash" void_ptr, "hash_len" int64_ptr) int32
-"cipher_init" 21 ("state" int32, "iv" void_ptr, "iv_len" int32) int32
-"cipher_update" 22 ("state" int32, "src" void_ptr, "src_len" int32, "dst" void_ptr, "dst_len" int64_ptr) int32
-"cipher_final" 23 ("state" int32, "src" void_ptr, "src_len" int32, "dst" void_ptr, "dst_len" int64_ptr) int32
-"cryp_obj_get_info" 24 ("obj" int32, TEE_ObjectInfo *info) int32
-"cryp_obj_restrict_usage" 25 ("obj" int32, "usage" int32) int32
-"cryp_obj_get_attr" 26 ("obj" int32, "attr_id" int32, "buffer" void_ptr, "size" int64_ptr) int32
-"cryp_obj_alloc" 27 ("obj_type" int32, "max_key_size" int32, "obj" int32_ptr) int32
-"cryp_obj_close" 28 ("obj" int32) int32
-"cryp_obj_reset" 29 ("obj" int32) int32
-"cryp_obj_populate" 30 ("obj" int32, struct utee_attribute *usr_attrs, "attr_count" int32) int32
-"cryp_obj_copy" 31 ("dst" int32, "src" int32) int32
-"cryp_derive_key" 32 ("state" int32, struct utee_attribute *usr_params, "param_count" int32, "derived_key" int32) int32
-"cryp_random_number_generate" 33 ("buf" void_ptr, "blen" int32) int32
-"authenc_init" 34 ("state" int32, "nonce" void_ptr, "nonce_len" int32, "tag_len" int32, "aad_len" int32, "payload_len" int32) int32
-"authenc_update_aad" 35 ("state" int32, "aad_data" void_ptr, "aad_data_len" int32) int32
-"authenc_update_payload" 36 ("state" int32, "src_data" void_ptr, "src_len" int32, "dst_data" void_ptr, "dst_len" int64_ptr) int32
-"authenc_enc_final" 37 ("state" int32, "src_data" void_ptr, "src_len" int32, "dst_data" void_ptr, "dst_len" int64_ptr, "tag" void_ptr, "tag_len" int64_ptr) int32
-"authenc_dec_final" 38 ("state" int32, "src_data" void_ptr, "src_len" int32, "dst_data" void_ptr, "dst_len" int64_ptr, "tag" void_ptr, "tag_len" int32) int32
-"asymm_operate" 39 ("state" int32, struct utee_attribute *usr_params, "num_params" int32, "src_data" void_ptr, "src_len" int32, "dst_data" void_ptr, "dst_len" int64_ptr) int32
-"asymm_verify" 40 ("state" int32, struct utee_attribute *usr_params, "num_params" int32, "data" void_ptr, "data_len" int32, "sig" void_ptr, "sig_len" int32) int32
-"storage_obj_open" 41 ("storage_id" int32, "object_id" void_ptr, "object_id_len" int32, "flags" int32, "obj" int32_ptr) int32
-"storage_obj_create" 42 ("storage_id" int32, "object_id" void_ptr, "object_id_len" int32, "flags" int32, "attr" int32, "data" void_ptr, "len" int32, "obj" int32_ptr) int32
-"storage_obj_del" 43 ("obj" int32) int32
-"storage_obj_rename" 44 ("obj" int32, "object_id" void_ptr, "object_id_len" int32) int32
-"storage_alloc_enum" 45 ("obj_enum" int32_ptr) int32
-"storage_free_enum" 46 ("obj_enum" int32) int32
-"storage_reset_enum" 47 ("obj_enum" int32) int32
-"storage_start_enum" 48 ("obj_enum" int32, "storage_id" int32) int32
-"storage_next_enum" 49 ("obj_enum" int32, TEE_ObjectInfo *info, "obj_id" void_ptr, "len" int64_ptr) int32
-"storage_obj_read" 50 ("obj" int32, "data" void_ptr, "len" int32, "count" int64_ptr) int32
-"storage_obj_write" 51 ("obj" int32, "data" void_ptr, "len" int32) int32
-"storage_obj_trunc" 52 ("obj" int32, "len" int32) int32
-"storage_obj_seek" 53 ("obj" int32, "offset" int32_t, "whence" int32) int32
-"obj_generate_key" 54 ("obj" int32, "key_size" int32, struct utee_attribute *usr_params, "param_count" int32) int32
+"unmask_cancellation" 10 ("old_mask" int32_ptr) int32
+"mask_cancellation" 11 ("old_mask" int32_ptr) int32
+"wait" 12 ("timeout" int32) int32
+"get_time" 13 ("cat" int32, "time" time_ptr) int32
+"set_ta_time" 14 ("time" time_ptr) int32
+"cryp_state_alloc" 15 ("algo" int32, "mode" int32, "key1" int32, "key2" int32, "state" int32_ptr [ __out_res__ ]) int32
+"cryp_state_copy" 16 ("dst" int32 [ __in_res__ ], "src" int32 [ __in_res__ ]) int32
+"cryp_state_free" 17 ("state" int32 [ __in_res__ ]) int32
+"hash_init" 18 ("state" int32 [ __in_res__ ], "iv" void_ptr [ __array__ ], "iv_len" int32 [ __array_len__ ]) int32
+"hash_update" 19 ("state" int32 [ __in_res__ ], "chunk" void_ptr [ __array__ ], "chunk_len" int32 [ __array_len__ ]) int32
+"hash_final" 20 ("state" int32 [ __in_res__ ], "chunk" void_ptr [ __array__ ], "chunk_len" int32 [ __array_len__ ], "hash" void_ptr [ __array__ ], "hash_len" int64_ptr [ __array_len__ ]) int32
+"cipher_init" 21 ("state" int32 [ __in_res__ ], "iv" void_ptr [ __array__ ], "iv_len" int32 [ __array_len__ ]) int32
+"cipher_update" 22 ("state" int32 [ __in_res__ ], "src" void_ptr [ __array__ ], "src_len" int32 [ __array_len__ ], "dst" void_ptr [ __array__ ], "dst_len" int64_ptr [ __array_len__ ]) int32
+"cipher_final" 23 ("state" int32 [ __in_res__ ], "src" void_ptr [ __array__ ], "src_len" int32 [ __array_len__ ], "dst" void_ptr [ __array__ ], "dst_len" int64_ptr [ __array_len__ ]) int32
+"cryp_obj_get_info" 24 ("obj" int32 [ __in_res__ ], "object_info" void_ptr) int32
+"cryp_obj_restrict_usage" 25 ("obj" int32 [ __in_res__ ], "usage" int32) int32
+"cryp_obj_get_attr" 26 ("obj" int32 [ __in_res__ ], "attr_id" int32, "buf" void_ptr [ __array__ ], "buf_len" int64_ptr [ __array_len__ ]) int32
+"cryp_obj_alloc" 27 ("obj_type" int32, "max_key_size" int32, "obj" int32_ptr [ __out_res__ ]) int32
+"cryp_obj_close" 28 ("obj" int32 [ __in_res__ ]) int32
+"cryp_obj_reset" 29 ("obj" int32 [ __in_res__ ]) int32
+"cryp_obj_populate" 30 ("obj" int32 [ __in_res__ ], "attrs" utee_attribute_ptr [ __array__ ], "attrs_count" int32 [ __array_len__ ]) int32
+"cryp_obj_copy" 31 ("dst" int32 [ __in_res__ ], "src" int32 [ __in_res__ ]) int32
+"cryp_derive_key" 32 ("state" int32 [ __in_res__ ], "params" utee_attribute_ptr [ __array__ ], "params_len" int32 [ __array_len__ ], "derived_key" int32) int32
+"cryp_random_number_generate" 33 ("buf" void_ptr [ __array__ ], "buf_len" int32 [ __array_len__ ]) int32
+"authenc_init" 34 ("state" int32 [ __in_res__ ], "nonce" void_ptr [ __array__ ], "nonce_len" int32 [ __array_len__ ], "tag_len" int32, "aad_len" int32, "payload_len" int32) int32
+"authenc_update_aad" 35 ("state" int32 [ __in_res__ ], "aad" void_ptr [ __array__ ], "aad_len" int32 [ __array_len__ ]) int32
+"authenc_update_payload" 36 ("state" int32 [ __in_res__ ], "src" void_ptr [ __array__ ], "src_len" int32 [ __array_len__ ], "dst" void_ptr [ __array__ ], "dst_len" int64_ptr [ __array_len__ ]) int32
+"authenc_enc_final" 37 ("state" int32 [ __in_res__ ], "src" void_ptr [ __array__ ], "src_len" int32 [ __array_len__ ], "dst" void_ptr [ __array__ ], "dst_len" int64_ptr [ __array_len__ ], "tag" void_ptr [ __array__ ], "tag_len" int64_ptr [ __array_len__ ]) int32
+"authenc_dec_final" 38 ("state" int32 [ __in_res__ ], "src" void_ptr [ __array__ ], "src_len" int32 [ __array_len__ ], "dst" void_ptr [ __array__ ], "dst_len" int64_ptr [ __array_len__ ], "tag" void_ptr [ __array__ ], "tag_len" int32 [__array_len__ ]) int32
+"asymm_operate" 39 ("state" int32 [ __in_res__ ], "params" utee_attribute_ptr [ __array__ ], "params_len" int32 [ __array_len__ ], "src" void_ptr [ __array__ ], "src_len" int32 [ __array_len__ ], "dst" void_ptr [ __array__ ], "dst_len" int64_ptr [ __array_len__ ]) int32
+"asymm_verify" 40 ("state" int32 [ __in_res__ ], "params" utee_attribute_ptr [ __array__ ], "params_len" int32 [ __array_len__ ], "data" void_ptr [ __array__ ], "data_len" int32 [ __array_len__ ], "sig" void_ptr [ __array__ ], "sig_len" int32 [ __array_len__ ]) int32
+"storage_obj_open" 41 ("storage_id" int32, "object_id" void_ptr [ __array__ ], "object_id_len" int32 [ __array_len__ ], "flags" int32, "obj" int32_ptr [ __out_res__ ]) int32
+"storage_obj_create" 42 ("storage_id" int32, "object_id" void_ptr [ __array__ ], "object_id_len" int32 [ __array_len__ ], "flags" int32, "attr" int32, "data" void_ptr [ __array__ ], "data_len" int32 [ __array_len__ ], "obj" int32_ptr [ __out_res__ ]) int32
+"storage_obj_del" 43 ("obj" int32 [ __in_res__ ]) int32
+"storage_obj_rename" 44 ("obj" int32 [ __in_res__ ], "object_id" void_ptr [ __array__ ], "object_id_len" int32 [ __array_len__ ]) int32
+"storage_alloc_enum" 45 ("obj_enum" int32_ptr [ __out_res__ ]) int32
+"storage_free_enum" 46 ("obj_enum" int32 [ __in_res__ ]) int32
+"storage_reset_enum" 47 ("obj_enum" int32 [ __in_res__ ]) int32
+"storage_start_enum" 48 ("obj_enum" int32 [ __in_res__ ], "storage_id" int32) int32
+"storage_next_enum" 49 ("obj_enum" int32 [ __in_res__ ], "info" void_ptr, "obj_id" void_ptr [__array__], "obj_id_len" int64_ptr [__array_len__]) int32
+"storage_obj_read" 50 ("obj" int32 [ __in_res__], "data" void_ptr [ __array__ ], "data_len" int32 [ __array_len__], "count" int64_ptr) int32
+"storage_obj_write" 51 ("obj" int32 [ __in_res__ ], "data" void_ptr [ __array__ ], "data_len" int32 [ __array_len__ ]) int32
+"storage_obj_trunc" 52 ("obj" int32 [ __in_res__ ], "len" int32) int32
+"storage_obj_seek" 53 ("obj" int32 [ __in_res__ ], "offset" int32, "whence" int32) int32
+"obj_generate_key" 54 ("obj" int32 [ __in_res__ ], "key_size" int32, "params" utee_attribute_ptr [ __array__ ], "param_len" int32 [ __array_len__ ]) int32
 "cache_operation" 70 ("va" void_ptr, "len" int32, "op" int32) int32
 """
 
--- tsmok/optee/syscall_parser.py
+++ tsmok/optee/syscall_parser.py
@@ -14,9 +14,14 @@ ta_call_parser = lark.Lark(r"""
 %import .sys_grammar (syscall, syscallname, syscallnumber, arg, argname)
 %import .sys_grammar (arglist, type, typename, type_options, type_opt)
 %import .sys_grammar (in_res, out_res, void_ptr, int32, int64, int32_ptr)
-%import .sys_grammar (int64_ptr, void, _in_, _out_, NUMBER)
+%import .sys_grammar (int64_ptr, void, _in_, _out_, array, array_len, NUMBER)
 
-%extend typename: "optee_utee_param_ptr" -> utee_param_ptr
+%extend typename: "utee_param_ptr" -> utee_param_ptr
+                 | "utee_attribute_ptr" -> utee_attribute_ptr
+                 | "utee_attribute_array" -> utee_attribute_array
+                 | "uuid_ptr" -> uuid_ptr
+                 | "object_info_ptr" -> object_info_ptr
+                 | "time_ptr" -> time_ptr
 
 %import common.WS
 %ignore WS
@@ -24,7 +29,7 @@ ta_call_parser = lark.Lark(r"""
 
 
 class UteeParamArg(syscall.Ptr, utee_args.OpteeUteeParamArgs):
-  """OPTEE TA Param pointer type for parser."""
+  """OPTEE UTEE Param pointer type for parser."""
 
   HDR_FMT = '<H'
   ARGDELIM = b'\xb7\xc9'
@@ -38,6 +43,10 @@ class UteeParamArg(syscall.Ptr, utee_args.OpteeUteeParamArgs):
 
   def value(self):
     return self.params
+
+  def reset(self):
+    self.params = ([utee_args.OpteeUteeParamNone()] *
+                   utee_args.OpteeUteeParamArgs.NUM_PARAMS)
 
   @classmethod
   def parse(cls, data):
@@ -116,14 +125,261 @@ class UteeParamArg(syscall.Ptr, utee_args.OpteeUteeParamArgs):
     return struct.pack(self.HDR_FMT, ptypes) + out
 
   def __str__(self):
-    out = 'OPTEE TA Param * ['
+    out = 'OPTEE UTEE Param * ['
     for p in self.params:
       out += str(p) + '; '
     out += '] ' + super().__str__()
     return out
 
+  def __len__(self):
+    raise NotImplementedError()
 
-class TaTransformer(parser.SyscallTransformer):
+  def load_from_mem(self, loader):
+    utee_args.OpteeUteeParamArgs.load_from_mem(self, loader, self.addr)
+
+
+class UteeAttributeArg(syscall.Ptr):
+  """OPTEE UTEE Attribute pointer type for parser."""
+
+  def __init__(self, attr=None):
+    syscall.Ptr.__init__(self, 0)
+    self.attr = attr
+
+  def arg(self):
+    return syscall.Ptr.arg(self)
+
+  def value(self):
+    return self.attr
+
+  def reset(self):
+    self.attr = None
+
+  @classmethod
+  def parse(cls, data):
+
+    sz = struct.calcsize(utee_args.OpteeUteeAttribute.TYPE_FMT)
+    if len(data) < sz:
+      raise ValueError('Wrong size of data')
+
+    atype = struct.unpack(utee_args.OpteeUteeAttribute.TYPE_FMT, data[:sz])[0]
+    if atype & utee_args.OPTEE_ATTR_BIT_VALUE:
+      attr = cls._setup_value_attr(atype, data[sz:])
+    else:
+      attr = cls._setup_memref_attr(atype, data[sz:])
+
+    return attr, utee_args.OpteeUteeAttribute.size_()
+
+  @classmethod
+  def _setup_value_attr(cls, atype, data):
+    sz = struct.calcsize(utee_args.OpteeUteeAttributeValue.BODY_FMT)
+    if len(data) < sz:
+      data += b'\x00' * (sz - len(data))
+    a, b = struct.unpack(utee_args.OpteeUteeAttributeValue.BODY_FMT, data[:sz])
+    attr = utee_args.OpteeUteeAttributeValue(atype, a, b)
+    return attr
+
+  @classmethod
+  def _setup_memref_attr(cls, atype, data):
+    attr = utee_args.OpteeUteeAttributeMemory(atype, data)
+    return attr
+
+  def load_to_mem(self, loader):
+    self.addr = self.attr.load_to_mem(loader, self.addr)
+
+  def __bytes__(self):
+    if not self.attr:
+      return b''
+
+    if isinstance(self.attr, utee_args.OpteeUteeAttributeValue):
+      return struct.pack('<' + self.attr.TYPE_FMT + self.attr.BODY_FMT,
+                         self.attr.atype, self.attr.a, self.attr.b)
+    else:
+      return struct.pack(self.attr.TYPE_FMT, self.attr.atype) + self.attr.data
+
+  def __str__(self):
+    out = 'OPTEE UTEE Attribute * ['
+    if self.attr:
+      out += str(self.attr)
+    out += ' ]' + super().__str__()
+    return out
+
+  def __len__(self):
+    raise NotImplementedError()
+
+  def load_from_mem(self, loader):
+    if self.addr:
+      self.attr = utee_args.OpteeUteeAttribute.create_from_mem(
+          loader, self.addr)
+
+
+class UteeAttributeArray(syscall.Array):
+  """OPTEE UTEE Attribute array type for parser."""
+
+  HDR_FMT = '<H'
+  ARGDELIM = b'\xb7\xc9'
+
+  def __init__(self, attrs=None):
+    syscall.Array.__init__(self, 0, 0)
+    self.attrs = attrs or []
+
+  def arg(self):
+    return syscall.Ptr.arg(self)
+
+  def value(self):
+    return self.attrs
+
+  def reset(self):
+    self.attrs = []
+
+  @classmethod
+  def parse(cls, data):
+
+    args_data = list(filter(None, data.split(cls.ARGDELIM)))
+
+    attrs = []
+    for adata in args_data:
+      sz = struct.calcsize(utee_args.OpteeUteeAttribute.TYPE_FMT)
+      if len(adata) < sz:
+        # not enough data to parse
+        continue
+
+      atype = struct.unpack(utee_args.OpteeUteeAttribute.TYPE_FMT,
+                            adata[:sz])[0]
+      if atype & utee_args.OPTEE_ATTR_BIT_VALUE:
+        attr = cls._setup_value_attr(atype, adata[sz:])
+      else:
+        attr = cls._setup_memref_attr(atype, adata[sz:])
+      attrs.append(attr)
+
+    return attrs, len(data)
+
+  @classmethod
+  def _setup_value_attr(cls, atype, data):
+    sz = struct.calcsize(utee_args.OpteeUteeAttribute.BODY_FMT)
+    if len(data) < sz:
+      data += b'\x00' * (sz - len(data))
+    a, b = struct.unpack(utee_args.OpteeUteeAttribute.BODY_FMT, data[:sz])
+    attr = utee_args.OpteeUteeAttributeValue(atype, a, b)
+    return attr
+
+  @classmethod
+  def _setup_memref_attr(cls, atype, data):
+    attr = utee_args.OpteeUteeAttributeMemory(atype, data)
+    return attr
+
+  def load_to_mem(self, loader):
+    if not callable(loader):
+      raise ValueError('loader argument is not a function')
+
+    adata = b''
+    for attr in self.attrs:
+      if isinstance(attr, utee_args.OpteeUteeAttributeMemory):
+        data = attr.data
+        if attr.size and not data:
+          data = b'\x00' * attr.size
+        if data:
+          attr.addr = loader(attr.addr, data)
+          if not attr.size:
+            attr.size = len(data)
+      adata += struct.pack(attr.FORMAT, *attr.values(), 0)
+
+    self.addr = loader(self.addr, adata)
+
+  def __bytes__(self):
+    if not self.attrs:
+      return b''
+
+    out = b''
+    for attr in self.attrs:
+      out += self.ARGDELIM
+      if isinstance(attr, utee_args.OpteeUteeAttributeValue):
+        out += struct.pack('<' + attr.TYPE_FMT + attr.BODY_FMT,
+                           attr.atype, attr.a, attr.b)
+      else:
+        out += struct.pack(attr.TYPE_FMT, attr.atype) + attr.data
+
+    return out
+
+  def __str__(self):
+    out = 'OPTEE UTEE Attribute array ['
+    for attr in self.attrs:
+      if attr:
+        out += str(attr) + ', '
+    out += ' ] ' + super().__str__()
+    return out
+
+  def __len__(self):
+    raise NotImplementedError()
+
+  def load_from_mem(self, loader):
+    if not self._size or not self.addr:
+      raise ValueError('Size is not specified!')
+
+    if not callable(loader):
+      raise ValueError('loader argument is not a function')
+
+    data = loader(self.addr, self._size)
+    sz = utee_args.OpteeUteeAttribute.size_()
+    off = 0
+    size = len(data)
+
+    self.attrs = []
+    while off < size:
+      attr = utee_args.OpteeUteeAttribute.create(data[off:off+sz])
+      off += sz
+
+      if isinstance(attr, utee_args.OpteeUteeAttributeMemory):
+        if attr.addr != 0 and attr.size != 0:
+          attr.data = loader(attr.addr, attr.size)
+      self.attrs.append(attr)
+
+
+class TimePtr(syscall.VoidPtr):
+  """OPTEE Time pointer type for parser."""
+
+  FMT = '<2I'
+
+  def __str__(self):
+    out = 'Time * ' + syscall.Ptr.__str__(self)
+    if self._data:
+      out += f' Data {self._data[:8].hex()}... '
+    return out
+
+  def load_from_mem(self, loader):
+    if self._size and self.addr:
+      self._data = loader(self.addr, struct.calcsize(self.FMT))
+
+
+class UuidPtr(syscall.VoidPtr):
+  """OPTEE UUID pointer type for parser."""
+
+  FMT = '<I2H8B'
+
+  def __str__(self):
+    out = 'Uuid * ' + syscall.Ptr.__str__(self)
+    if self._data:
+      out += f' Data {self._data[:8].hex()}... '
+    return out
+
+  def load_from_mem(self, loader):
+    if self._size and self.addr:
+      self._data = loader(self.addr, struct.calcsize(self.FMT))
+
+
+class ObjectInfoPtr(syscall.VoidPtr):
+  """OPTEE ObjectInfo pointer type for parser."""
+
+  FMT = '<7I'
+
+  def __str__(self):
+    out = 'ObjectInfo * ' + syscall.Ptr.__str__(self)
+    if self._data:
+      out += f' Data {self._data[:8].hex()}... '
+    return out
+
+
+class OpteeArgsTransformer(parser.SyscallTransformer):
+  """OPTEE Syscall transformer."""
 
   def __init__(self):
     parser.SyscallTransformer.__init__(self)
@@ -131,10 +387,25 @@ class TaTransformer(parser.SyscallTransformer):
   def utee_param_ptr(self, *args):
     return UteeParamArg
 
+  def utee_attribute_ptr(self, *args):
+    return UteeAttributeArg
+
+  def utee_attribute_array(self, *args):
+    return UteeAttributeArray
+
+  def time_ptr(self, *args):
+    return TimePtr
+
+  def object_info_ptr(self, *args):
+    return ObjectInfoPtr
+
+  def uuid_ptr(self, *args):
+    return UuidPtr
+
 
 def parse(data):
   try:
     tree = ta_call_parser.parse(data)
-    return TaTransformer().transform(tree)
+    return OpteeArgsTransformer().transform(tree)
   except lark.GrammarError:
     raise error.Error('Failed to parse')
